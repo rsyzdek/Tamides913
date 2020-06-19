a91b2dd03c2701c694b13401034f6b819db2ef9b
Robert Syzdek <rsyzdek@9bits.pl>
2020-06-19 13:23:54 +0200

    +++

--- app/src/main/java/pl/tamides/tamides913/Scripts.kt
+++ app/src/main/java/pl/tamides/tamides913/Scripts.kt
@@ -4,29 +4,27 @@ class Scripts {
     companion object {
         val logInScript = """
 (function () {
-    var login = "${Cfg.login}";
-    var password = "${Cfg.password}";
     var inputFields = document.getElementsByClassName('form_input_field');
     var i;
     
     for (i = 0; i < inputFields.length; i++) {
-        if (inputFields[i].name == "login") {
+        var loginInput = inputFields[i];
+        
+        if (loginInput.name == "login") {
             break;
         }
     }
-    
-    var loginInput = inputFields[i];
     
     for (i = 0; i < inputFields.length; i++) {
-        if (inputFields[i].name == "pass") {
+        var passwordInput = inputFields[i];
+    
+        if (passwordInput.name == "pass") {
             break;
         }
     }
     
-    var passwordInput = inputFields[i];
-    
-    loginInput.value = login;
-    passwordInput.value = password;
+    loginInput.value = "${Cfg.login}";
+    passwordInput.value = "${Cfg.password}";
     
     var buttons = document.getElementsByClassName('btn_enter');
     
