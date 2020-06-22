11a7c1d3bc1e08f682e5002dacf219575b9ae4fc
Robert Syzdek <rsyzdek@9bits.pl>
2020-06-22 13:05:52 +0200

    +++

---
+++ app/src/main/java/pl/tamides/tamides913/Pages.kt
@@ -0,0 +1,10 @@
+package pl.tamides.tamides913
+
+class Pages {
+    companion object {
+        val loginPage = "https://www.lordswm.com/"
+        val homePage = "https://www.lordswm.com/home.php"
+        val mapPage = "https://www.lordswm.com/map.php"
+        val inventoryPage = "https://www.lordswm.com/inventory.php"
+    }
+}
